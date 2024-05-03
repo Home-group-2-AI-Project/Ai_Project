3430ba5e168d511319be6164438805b158a32b3a
Julian Rodriguez <est.julianan.rodri2@unimilitar.edu.co>
2024-05-02 22:01:36 -0500

    update:slackmessages

--- app/slack_config/SlackMesagges.py
+++ app/slack_config/SlackMesagges.py
@@ -13,8 +13,7 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/eve
 client = slack.WebClient(token=os.environ['SLACK_TOKEN']) # Create a slack client
 BOT_ID = client.api_call("auth.test")['user_id'] # Get the bot id
 
-def get_channels():
-    
+def get_channels():   
     all_channels_response = client.conversations_list(types="public_channel")
     if all_channels_response['ok']:
         all_channels = all_channels_response['channels']
@@ -23,10 +22,9 @@ def get_channels():
         for channel in all_channels:
             channel_info = {
                 'name_normalized': channel['name_normalized'],
-                'id': channel['id']
+                'id': channel['id'],
             }
-            channel_info_list.append(channel_info)
-        
+            channel_info_list.append(channel_info) 
         return channel_info_list
     else:
         print(f"Error getting channels: {all_channels_response['error']}")
@@ -34,7 +32,6 @@ def get_channels():
 
 
 channels = get_channels()
-print(channels)
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -42,17 +39,29 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text') # en este texto se encuentra el mensaje que el usuario envió
+    ts = event.get('event_ts')
     #info = client.team_info() # Obtener información del slack en general 
+    channel_name = [channel['name_normalized'] for channel in channels if channel['id'] == channel_id]
+
     conversation_history = []
     mensajes = []
      # Ensure it's a message from a user (not the bot itself) and avoid sensitive information
     if BOT_ID != user_id:
+        print(f'in channel {channel_id}')
         # Iterate over each channel and get messages
-        for channel_info in channels:
-            channel_id = channel_info['id']
-            channel_name = channel_info['name_normalized']  # Obtener el nombre del canal
-            # Use conversations.history API call to get channel messages
+        # Use conversations.history API call to get channel messages
+        try:
+            csv_file_path = f'app/slack_config/Data_messages/channel_{channel_id}_messages.csv'
+            existing_data = pd.read_csv(csv_file_path)         
+            new_row = pd.DataFrame([{'channel_name': channel_name[0], 'user': user_id, 'text': text, 'ts': ts}])
+            updated_data = pd.concat([new_row,existing_data], ignore_index=True, sort=False)
+            updated_data.to_csv(csv_file_path, index=False)
+            return
+        except Exception as e:
+            print(f"Error opening file for channel {channel_name}: {e}")
+            print(f"Getting messages for channel {channel_name}")
             result = client.conversations_history(channel=channel_id)
+        
             if result['ok']:
                 conversation_history = result["messages"]
                 mensajes = []
@@ -61,9 +70,9 @@ def message(payload):
                         user = mensaje['user']
                         text = mensaje['text']
                         ts = mensaje['ts']
-                    
+
                     # Agregar el nombre del canal al mensaje
-                        mensajes.append({'channel_name': channel_name, 'user': user, 'text': text, 'ts': ts})
+                        mensajes.append({'channel_name': channel_name[0], 'user': user, 'text': text, 'ts': ts})
                     else:
                         print("Mensaje no contiene información de usuario:", mensaje)                        
                 # Save messages to CSV
@@ -71,7 +80,7 @@ def message(payload):
                     df.to_csv(f'app/slack_config/Data_messages/channel_{channel_id}_messages.csv', index=False)
             else:
                 print(f"Error getting messages for channel {channel_name}: {result['error']}")
-
+            
 
 @app.route('/sentiment',methods=['Post'])
 def sentiment():
