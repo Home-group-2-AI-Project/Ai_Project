b79e5bbcae1cc7d1e7f59246d55caa4c01e0bba4
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-05-03 16:37:14 -0500

    Add class interface to communicate with openAI models
    - Add a class with appropiate methods to communicate to openAI models
      based on the command requests
    - Refactor the simple app file

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,7 @@
 import os
 import openai
 
+from open_ai_connection import OpenAIConnection
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
@@ -10,17 +11,13 @@ load_dotenv()
 
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_SIGNING_SECRET = os.environ.get("SLACK_SIGNING_SECRET")
-OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
-
 
 app = App(
     token=SLACK_BOT_TOKEN,
     signing_secret=SLACK_SIGNING_SECRET
 )
 
-client = openai.OpenAI(
-    api_key=OPENAI_API_KEY
-)
+openai_connection = OpenAIConnection()
 
 
 @app.command("/chatgpt")
@@ -31,20 +28,10 @@ def command(ack, say, command):
     # Extract the user's message
     text = command['text']
 
-    # Query the OpenAI API
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "Eres un asistente de bot en slack que se encarga de responder a los usuarios."},
-            {"role": "user", "content": text}
-        ]
-    )
-
-    # Extract the AI's response
-    response_text = response.choices[0].message.content
+    reply = OpenAIConnection.chat_gpt(text)
 
     # Post the user's message and the response back to the same channel
-    say(f"*Message:* {text}\n*Response:* {response_text}\n----------------")
+    say(f"*Respuesta:* {reply}\n----------------")
 
 
 @app.event("app_home_opened")
@@ -106,9 +93,11 @@ handler = SlackRequestHandler(app)
 def slack_events():
     return handler.handle(request)
 
+
 @flask_app.route("/slack/commands", methods=["POST"])
 def slack_commands():
     return handler.handle(request)
+
 
 @flask_app.route("/slack/install", methods=["GET"])
 def install():
