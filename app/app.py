33d92b4fb64f0eb1300ad28b08dbc42793fc1885
DavedCV <97773641+DavedCV@users.noreply.github.com>
2024-05-05 18:14:33 -0500

    add changes of names and structure

--- app/app.py
+++ app/app.py
@@ -5,11 +5,12 @@ import re
 
 import pandas as pd
 
-from open_ai_connection import OpenAIConnection
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, request
+from slack_utils.open_ai_connection import OpenAIConnection
+from slack_utils.get_channels import get_channels
 
 load_dotenv()
 
@@ -22,26 +23,6 @@ app = App(
 )
 
 openai_connection = OpenAIConnection()
-
-def get_channels(app):
-    try:
-        result = app.client.conversations_list(types="public_channel")
-        if result['ok']:
-            all_channels = result['channels']
-            channel_info_list = []
-            for channel in all_channels:
-                channel_info = {
-                    'name_normalized': channel['name_normalized'],
-                    'id': channel['id'],
-                }
-                channel_info_list.append(channel_info)
-            return channel_info_list
-        else:
-            print(f"Error getting channels: {result['error']}")
-            return []  # Return empty list on error
-    except Exception as e:
-        print(f"Error: {e}")
-
 channels = get_channels(app)
 
 ################################################################################
